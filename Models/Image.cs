c3fdab9c5be77dcc61f0c7188cdee06105e09d5e
Віктор Шевченко <kaspianturbo@gmail.com>
2020-09-06 19:36:33 +0300

    Created models and added MVC to project

---
+++ Models/Image.cs
@@ -0,0 +1,8 @@
+namespace DeepDiveIntoEnglishLanding.Models
+{
+    public class Image
+    {
+        public int Id { get; set; }
+        public string Source { get; set; }
+    }
+}
---
+++ Models/Review.cs
@@ -0,0 +1,10 @@
+namespace DeepDiveIntoEnglishLanding.Models
+{
+    public class Review
+    {
+        public int Id { get; set; }
+        public string Reviewer { get; set; }
+        public string Text { get; set; }
+        public string ImageSource { get; set; }
+    }
+}
